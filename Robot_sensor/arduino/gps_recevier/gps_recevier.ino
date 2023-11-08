f43b76d971f10b7ac9f223cec5f4e2db6ab64f4e
JD-edu <96219601+JD-edu@users.noreply.github.com>
2023-11-08 15:27:37 +0900

    Nov 8 5

---
+++ Robot_sensor/arduino/gps_recevier/gps_recevier.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial gps(11, 12)
+
+void setup() {
+  // put your setup code here, to run once:
+  Serila.begin(9600);
+  gps.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(gps.available()){
+    Serial.write(gps.read());
+  }
+
+}
