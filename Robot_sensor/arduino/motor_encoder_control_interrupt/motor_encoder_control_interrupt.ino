fc37edb8043cbba67f5919d69e6b97a145d2827d
JD-edu <96219601+JD-edu@users.noreply.github.com>
2023-11-08 20:18:25 +0900

    Nov8 7

---
+++ Robot_sensor/arduino/motor_encoder_control_interrupt/motor_encoder_control_interrupt.ino
@@ -0,0 +1,30 @@
+* The purpose of this code is to count the ouput pulses or 
+ * the encoder outputs as you rotate the Motor shaft. You can run the 
+ * same code on the Arduino Uno, Arduino Nano, Arduino Mega, etc.
+ */
+#define Encoder_output_A 2 // pin2 of the Arduino
+#define Encoder_output_B 3 // pin 3 of the Arduino
+// these two pins has the hardware interrupts as well. 
+ 
+int Count_pulses = 0;
+void setup() {
+Serial.begin(9600); // activates the serial communication
+pinMode(Encoder_output_A,INPUT); // sets the Encoder_output_A pin as the input
+pinMode(Encoder_output_B,INPUT); // sets the Encoder_output_B pin as the input
+attachInterrupt(digitalPinToInterrupt(Encoder_output_A),DC_Motor_Encoder,RISING);
+}
+ 
+void loop() {
+  Serial.println("Result: ");
+  Serial.println(Count_pulses); 
+}
+ 
+void DC_Motor_Encoder(){
+  int b = digitalRead(Encoder_output_B);
+  if(b > 0){
+    Count_pulses++;
+  }
+  else{
+    Count_pulses--;
+  }
+}
