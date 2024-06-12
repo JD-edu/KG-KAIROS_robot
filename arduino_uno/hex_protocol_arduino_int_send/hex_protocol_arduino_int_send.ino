8a66bd28bcd4d58d389256adf963a5fdedabd219
JD-edu <96219601+JD-edu@users.noreply.github.com>
2024-06-12 17:05:08 +0900

    June 12th -3

---
+++ arduino_uno/hex_protocol_arduino_int_send/hex_protocol_arduino_int_send.ino
@@ -0,0 +1,29 @@
+#include <SoftwareSerial.h>
+// Using softserial as debugging. Arduino Uno has only 1 serial port. 
+SoftwareSerial Serial2(7,8);
+
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(115200);
+  Serial2.println("Start...");
+}
+
+long data = 0;  // 4 bytes       
+
+void loop(){ 
+
+  data += 100;
+  Serial2.print( "data:" );    // print for debugging 
+  Serial2.print( data );
+ 
+  byte data_buffer[5] = {0};   // Command packer container 
+  data_buffer[0] = 0xf5;
+  data_buffer[1] = (data >> 24) & 0xff;     // long 4 bytes as byte array 
+  data_buffer[2] = (data >> 16) & 0xff;    
+  data_buffer[3] = (data >> 8) & 0xff;
+  data_buffer[4] = data & 0xff;
+ 
+  Serial.write(data_buffer, 5);  // Sending command packets 
+  delay(1000);
+
+}
---
+++ arduino_uno/hex_protocol_arduino_int_send/hex_protocol_python_int_receive.py
@@ -0,0 +1,51 @@
+import serial
+import threading 
+import time 
+
+class JDamr(object):
+    def __init__(self, com="COM12"):
+        self.ser = serial.Serial(com, 115200)
+        if self.ser.isOpen():
+            print("JDamr serial port opened!")
+        else:
+            print("Can't open JDamr serial port!")
+        time.sleep(1)
+    
+    def receive_data(self):     
+        self.ser.flushInput()
+       
+        while True:
+            head = bytearray(self.ser.read())[0]
+            if head == 0xf5:
+                payload = [] 
+                for i in range(4):
+                    value = bytearray(self.ser.read())[0]
+                    payload.append(value)
+                self.parse_cmd(payload)
+
+    def receive_thread(self):
+        try:
+            task_name = "serial_thread"
+            rx_task = threading.Thread(target=self.receive_data, name=task_name)
+            rx_task.setDaemon(True)
+            rx_task.start()
+            print("Start serial receive thread ")
+            time.sleep(.05)
+        except:
+            pass
+
+    def parse_cmd(self, payload):
+        print(payload)
+        encode1 =int.from_bytes(payload, byteorder="big")
+        print(encode1)
+
+if __name__ == '__main__':
+    com = 'COM12'
+    bot = JDamr(com)
+    time.sleep(1)
+    bot.receive_thread()
+   
+    while True:
+       pass
+
+
